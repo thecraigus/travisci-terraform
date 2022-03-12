29eefb11a7fe2d27710fc8e8fd734781b5a70229
Craig Cartlidge <c0c0lfx@uk.wal-mart.com>
2022-03-12 18:41:54 +0000

    attempt2

--- main.tf
+++ main.tf
@@ -45,10 +45,10 @@ resource "aws_vpc" "craigs_vpc" {
 
 }
 
-/*
+
 locals {
   cisco_asav_name       = "CiscoASAv"          # Just for fun, name your ASAv anything you'd like!
-  my_public_ip          = "1.2.3.4/32"         # Update this to your public IP when deploying
+  my_public_ip          = "2.40.27.115/32"         # Update this to your public IP when deploying
   ssh_key_name          = "cisco_asav_keypair" # Update only if you've created an SSH key with a different name than cisco_asav_keypair
   asav_public_facing_ip = "172.16.20.10"
 }
@@ -206,7 +206,7 @@ resource "aws_eip" "cisco_asav_elastic_public_ip" {
 # Build the ASAv
 resource "aws_instance" "cisco_asav" {
   # This AMI is only valid in us-east-1 region, with this specific instance type
-  ami           = "ami-01426821ff0f3a449"
+  ami           = "ami-0edeb8fb244c4d161"
   instance_type = "c5.xlarge"
   key_name      = local.ssh_key_name
 
@@ -230,4 +230,3 @@ resource "aws_instance" "cisco_asav" {
 output "asav_public_ip" {
   value = aws_eip.cisco_asav_elastic_public_ip.public_ip
 }
-*/
